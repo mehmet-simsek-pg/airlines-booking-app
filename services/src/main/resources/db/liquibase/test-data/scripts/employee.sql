3306d3d049b3ea2e19fd205a6731a7eecdcd8533
Mehmet Simsek <simsekmehmet00@gmail.com>
2024-04-16 13:08:59 +0200

    employee test data updated

--- services/src/main/resources/db/liquibase/test-data/scripts/employee.sql
+++ services/src/main/resources/db/liquibase/test-data/scripts/employee.sql
@@ -1,8 +1,8 @@
-INSERT INTO employee VALUES(1,'Test User1',11,30,'testuser001@example.com','+918530324018');
-INSERT INTO employee VALUES(2,'Test User2',12,26,'testuser002@example.com','+447911123456');
-INSERT INTO employee VALUES(3,'Test User3',13,36,'testuser003@example.com','+917504681201');
-INSERT INTO employee VALUES(4,'Test User4',14,33,'testuser004@example.com','6465554468');
-INSERT INTO employee VALUES(5,'Test User5',15,20,'testuser005@example.com','+45886443210');
-INSERT INTO employee VALUES(6,'Test User6',16,31,'testuser006@example.com','+448000751234');
-INSERT INTO employee VALUES(7,'Test User7',17,32,'testuser007@example.com','+919785425154');
-INSERT INTO employee VALUES(8,'Test User8',18,31,'testuser008@example.com','+918211954901');
+INSERT INTO employee VALUES(1,'Test User1',11,30,'testuser001@example.com','+918530324018',1);
+INSERT INTO employee VALUES(2,'Test User2',12,26,'testuser002@example.com','+447911123456',1);
+INSERT INTO employee VALUES(3,'Test User3',13,36,'testuser003@example.com','+917504681201',1);
+INSERT INTO employee VALUES(4,'Test User4',14,33,'testuser004@example.com','6465554468',2);
+INSERT INTO employee VALUES(5,'Test User5',15,20,'testuser005@example.com','+45886443210',2);
+INSERT INTO employee VALUES(6,'Test User6',16,31,'testuser006@example.com','+448000751234',3);
+INSERT INTO employee VALUES(7,'Test User7',17,32,'testuser007@example.com','+919785425154',4);
+INSERT INTO employee VALUES(8,'Test User8',18,31,'testuser008@example.com','+918211954901',5);
